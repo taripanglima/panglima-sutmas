7c22cbc995b98af23f19ad16be0471c5ca19a298
taripanglima <taripanglima@gmail.com>
2026-09-08 07:01:55 +0800

    Add files via upload

---
+++ assets/sw.js
@@ -0,0 +1,10 @@
+const CACHE="gps-v1";
+const CORE=["./","./index.html","./assets/images/logo.png","./assets/images/wan.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{
+    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
+  }).catch(()=>cached)));
+});
